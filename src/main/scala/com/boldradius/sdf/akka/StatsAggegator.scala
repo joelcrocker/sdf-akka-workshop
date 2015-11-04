cd329ac733765e420a4844fb3f39de8e9e643718
Aleksey Nikiforov <lexn82@gmail.com>
2015-11-04 16:05:20 -0500

    More work.

--- src/main/scala/com/boldradius/sdf/akka/StatsAggegator.scala
+++ src/main/scala/com/boldradius/sdf/akka/StatsAggegator.scala
@@ -6,10 +6,17 @@ import akka.actor._
 object StatsAggegator {
   def props = Props(new StatsAggegator)
 
-  case class BrowserStats(requests: Map[String, Long], users: Map[String, Long])
+  case class BrowserStats(requests: Map[String, Long], users: Map[String, Long]) {
+    def topTwoBrowsers: Seq[(String, Long)] = {
+      users.toSeq.sortBy { case (browser, userCount) => userCount }.takeRight(2).reverse
+    }
+  }
+  case class UrlStats(totalDuration: Long, visitCount: Long) {
+    def average: Double = totalDuration.toDouble / visitCount
+  }
+  type StatsPerUrl = Map[String, UrlStats]
 
-  def statsPerBrowser(oldStats: BrowserStats, sessionHistory: Seq[Request])
-  : BrowserStats = {
+  def statsPerBrowser(oldStats: BrowserStats, sessionHistory: Seq[Request]) : BrowserStats = {
     val sessionRequestStats = sessionHistory.groupBy(_.browser).map {
       case (browser, requests) => browser -> requests.size
     }
@@ -18,9 +25,20 @@ object StatsAggegator {
     var newUserStats = oldStats.users withDefaultValue 0L
     for ((browser, count) <- sessionRequestStats) {
       newRequestStats += browser -> (newRequestStats(browser) + count)
-      newUserStats += browser -> (newRequestStats(browser) + 1)
+      newUserStats += browser -> (newUserStats(browser) + 1)
     }
     BrowserStats(newRequestStats, newUserStats)
+  }
+
+  def statsPerUrl(oldStats: StatsPerUrl, sessionHistory: Seq[Request]): StatsPerUrl = {
+    var newStats = oldStats withDefaultValue UrlStats(0, 0)
+    for (Seq(prev, next) <- sessionHistory.sliding(2)) {
+      val duration = next.timestamp - prev.timestamp
+      val url = prev.url
+      val urlStats = newStats(url)
+      newStats += url -> urlStats.copy(urlStats.totalDuration + duration, urlStats.visitCount + 1)
+    }
+    newStats
   }
 }
 
