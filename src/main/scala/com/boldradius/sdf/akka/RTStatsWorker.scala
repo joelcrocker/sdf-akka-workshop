e10179f93136dcfaf61e12c9fce893b567f74324
Joel Crocker <jcrocker@pagerduty.com>
2015-11-06 11:31:21 -0500

    Add realtime session stats, lightly tested, not wired into app

---
+++ src/main/scala/com/boldradius/sdf/akka/RTStatsWorker.scala
@@ -0,0 +1,89 @@
+package com.boldradius.sdf.akka
+
+import akka.actor._
+
+import RequestConsumer.{GetSessionMap, SessionMapResponse}
+import com.boldradius.sdf.akka.RTStatsWorker.CollectingState
+import com.boldradius.sdf.akka.SessionTracker.{GetSessionStats, SessionStats}
+
+object RTStatsWorker {
+  import RTStatsQuerier._
+
+  def props(consumer: ActorRef) = Props(new RTStatsWorker(consumer))
+
+  case class CollectingState(
+    recipient: ActorRef,
+    sessionMap: Map[Long, ActorRef],
+    responseBuilder: (Map[String, Long]) => FieldCountResponse
+  )
+}
+
+class RTStatsWorker(consumer: ActorRef) extends Actor with ActorLogging with Stash {
+  import RTStatsQuerier._
+
+  consumer ! GetSessionMap
+
+  var sessionCount = 0
+  var requestFieldCounts = Map[String, Long]() withDefaultValue 0L
+
+  def receive: Receive = initializing
+
+  def initializing: Receive = {
+    case SessionMapResponse(sessionMap) =>
+      unstashAll()
+      context.become(ready(sessionMap))
+    case _ =>
+      stash()
+  }
+
+  def ready(sessionMap: Map[Long, ActorRef]): Receive = {
+    case GetTotalSessionCount =>
+      sender ! TotalSessionCount(sessionMap.size)
+
+    case msg @ GetSessionCountPerUrl =>
+      val collectingState = CollectingState(sender(), sessionMap, SessionCountPerUrl)
+      collectSessions(collectingState)
+      context.become(collectingUrlStats(collectingState))
+
+    case msg @ GetSessionCountPerBrowser =>
+      val collectingState = CollectingState(sender(), sessionMap, SessionCountPerBrowser)
+      collectSessions(collectingState)
+      context.become(collectingBrowserStats(collectingState))
+  }
+
+  def collectingUrlStats = collectingFieldStats(_.url)_
+
+  def collectingBrowserStats = collectingFieldStats(_.browser)_
+
+  def collectingFieldStats(f: Request => String)(
+    state: CollectingState
+  ): Receive = {
+    case SessionStats(sessionId, requestHistory) =>
+      updateFieldCounts(f, requestHistory)
+      sessionCount += 1
+      checkCollectedCounts(state)
+  }
+
+  def fieldsForSession(f: Request => String, history: Seq[Request]): Seq[String] = {
+    history.map(f).distinct
+  }
+
+  def updateFieldCounts(f: Request => String, history: Seq[Request]): Unit = {
+    val valuesForSession = fieldsForSession(f, history)
+    for (value <- valuesForSession) {
+      requestFieldCounts += value -> (requestFieldCounts(value) + 1)
+    }
+  }
+
+  def collectSessions(collectingState: CollectingState): Unit = {
+    collectingState.sessionMap.valuesIterator.foreach(_ ! GetSessionStats)
+    checkCollectedCounts(collectingState)
+  }
+
+  def checkCollectedCounts(collectingState: CollectingState): Unit = {
+    if (sessionCount >= collectingState.sessionMap.size) {
+      collectingState.recipient ! collectingState.responseBuilder(requestFieldCounts)
+      context.stop(self)
+    }
+  }
+}
---
+++ src/test/scala/com/boldradius/sdf/akka/RTStatsQuerierSpec.scala
@@ -0,0 +1,41 @@
+package com.boldradius.sdf.akka
+
+import akka.testkit._
+import com.boldradius.sdf.akka.RTStatsQuerier._
+
+class RTStatsQuerierSpec extends BaseAkkaSpec {
+  // TODO: add tests for non-empty session counts.
+
+  "Querying for total session count with no trackers" should {
+    "return the correct result" in {
+      val consumer = system.actorOf(RequestConsumer.props(new Settings()))
+      val client = TestProbe()
+
+      val querier = system.actorOf(RTStatsQuerier.props(consumer))
+      querier.tell(GetTotalSessionCount, client.ref)
+      client.expectMsg(TotalSessionCount(0))
+    }
+  }
+
+  "Querying for sessions per url with no trackers" should {
+    "return the correct result" in {
+      val consumer = system.actorOf(RequestConsumer.props(new Settings()))
+      val client = TestProbe()
+
+      val querier = system.actorOf(RTStatsQuerier.props(consumer))
+      querier.tell(GetSessionCountPerUrl, client.ref)
+      client.expectMsg(SessionCountPerUrl(Map()))
+    }
+  }
+
+  "Querying for sessions per browser with no trackers" should {
+    "return the correct result" in {
+      val consumer = system.actorOf(RequestConsumer.props(new Settings()))
+      val client = TestProbe()
+
+      val querier = system.actorOf(RTStatsQuerier.props(consumer))
+      querier.tell(GetSessionCountPerBrowser, client.ref)
+      client.expectMsg(SessionCountPerBrowser(Map()))
+    }
+  }
+}
